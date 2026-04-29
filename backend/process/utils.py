43ac84468c5b1283697360a0f193ee13f9f8da1d
Cesar Nunez <ca.nunezhuaman@gmail.com>
2026-04-28 22:43:00 -0500

    Update utils.py to obtain hidden data from congresistas

--- backend/process/utils.py
+++ backend/process/utils.py
@@ -59,11 +59,10 @@ def gen_congresistas_df(session: Session) -> None:
     )
     motions_congresistas = (
         session.query(RawMotion.congresistas)
-        .filter(RawBill.last_update)
+        .filter(RawMotion.last_update)
         .distinct()
         .all()
     )
-
     all_cong = []
 
     for (json_str,) in bills_congresistas + motions_congresistas:
@@ -75,7 +74,9 @@ def gen_congresistas_df(session: Session) -> None:
             continue
 
     unique_by_congresista = {
-        d["congresistaId"]: d for d in all_cong if "congresistaId" in d
+        d["congresistaId"]: d
+        for d in all_cong
+        if ("congresistaId" in d) and ("dni" in d)
     }
 
     df = pl.DataFrame(list(unique_by_congresista.values()))
